ac9dc7681d1296060534b5114aff2ad22b8527ed
Maidarman <Maidarman@10.5.250.51>
2016-03-18 23:15:49 +0700

    menambah class Tugas

--- SisfoProjectManagement/src/sisfoprojectmanagement/Tugas.java
+++ SisfoProjectManagement/src/sisfoprojectmanagement/Tugas.java
@@ -4,11 +4,55 @@
  * and open the template in the editor.
  */
 package sisfoprojectmanagement;
-
+import java.util.Date;
 /**
  *
  * @author Maidarman
  */
 public class Tugas {
-    
+        private Programer pelaksana;
+	private String nama;
+	private String detail;
+	private boolean status;
+	private Date deadline;
+
+	public void setPelaksana (Programer p){
+		pelaksana = p;
+	}
+
+	public Programer getPelaksana (){
+		return pelaksana;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+
+	public void setDetail (String detail){
+		this.detail = detail;
+	}
+
+	public String getDetail (){
+		return detail;
+	}
+
+	public void setStatus (boolean status){
+		this.status = status;
+	}
+
+	public boolean getStatus (){
+		return status;
+	}
+
+	public void setDeadline (Date date){
+		deadline = date;
+	}
+
+	public Date getDeadline (){
+		return deadline;
+	}
 }
